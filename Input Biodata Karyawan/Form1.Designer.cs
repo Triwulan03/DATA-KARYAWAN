ecb7cfceca5f130258e6b316af3267cb21930f08
Triwulan03 <113260869+Triwulan03@users.noreply.github.com>
2022-09-13 16:59:42 +0700

    Biodata Karyawan

---
+++ Input Biodata Karyawan/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace Input_Biodata_Karyawan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtTempat = new System.Windows.Forms.TextBox();
+            this.txtAgama = new System.Windows.Forms.TextBox();
+            this.txtNIK = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.rb_pr = new System.Windows.Forms.RadioButton();
+            this.rb_lk = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cmbJabatan = new System.Windows.Forms.ComboBox();
+            this.btnProses = new System.Windows.Forms.Button();
+            this.btnBersih = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jenis Kelamin";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tempat, tanggal lahir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(115, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Agama";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(115, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "NIK";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(115, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Jabatan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(115, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "ID Karyawan";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(306, 25);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(237, 22);
+            this.txtNama.TabIndex = 7;
+            // 
+            // txtTempat
+            // 
+            this.txtTempat.Location = new System.Drawing.Point(306, 99);
+            this.txtTempat.Name = "txtTempat";
+            this.txtTempat.Size = new System.Drawing.Size(237, 22);
+            this.txtTempat.TabIndex = 8;
+            // 
+            // txtAgama
+            // 
+            this.txtAgama.Location = new System.Drawing.Point(306, 140);
+            this.txtAgama.Name = "txtAgama";
+            this.txtAgama.Size = new System.Drawing.Size(237, 22);
+            this.txtAgama.TabIndex = 9;
+            // 
+            // txtNIK
+            // 
+            this.txtNIK.Location = new System.Drawing.Point(306, 191);
+            this.txtNIK.Name = "txtNIK";
+            this.txtNIK.Size = new System.Drawing.Size(237, 22);
+            this.txtNIK.TabIndex = 11;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(306, 264);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(237, 22);
+            this.txtID.TabIndex = 12;
+            // 
+            // rb_pr
+            // 
+            this.rb_pr.AutoSize = true;
+            this.rb_pr.Location = new System.Drawing.Point(306, 64);
+            this.rb_pr.Name = "rb_pr";
+            this.rb_pr.Size = new System.Drawing.Size(102, 21);
+            this.rb_pr.TabIndex = 13;
+            this.rb_pr.TabStop = true;
+            this.rb_pr.Text = "Perempuan";
+            this.rb_pr.UseVisualStyleBackColor = true;
+            // 
+            // rb_lk
+            // 
+            this.rb_lk.AutoSize = true;
+            this.rb_lk.Location = new System.Drawing.Point(433, 64);
+            this.rb_lk.Name = "rb_lk";
+            this.rb_lk.Size = new System.Drawing.Size(94, 21);
+            this.rb_lk.TabIndex = 14;
+            this.rb_lk.TabStop = true;
+            this.rb_lk.Text = "Laki - Laki";
+            this.rb_lk.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(306, 292);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 116);
+            this.listBox1.TabIndex = 15;
+            // 
+            // cmbJabatan
+            // 
+            this.cmbJabatan.FormattingEnabled = true;
+            this.cmbJabatan.Items.AddRange(new object[] {
+            "Direktur Personalia",
+            "Manajer Pemasaran",
+            "Manajer Pabrik",
+            "Staff"});
+            this.cmbJabatan.Location = new System.Drawing.Point(306, 226);
+            this.cmbJabatan.Name = "cmbJabatan";
+            this.cmbJabatan.Size = new System.Drawing.Size(237, 24);
+            this.cmbJabatan.TabIndex = 16;
+            // 
+            // btnProses
+            // 
+            this.btnProses.Location = new System.Drawing.Point(118, 423);
+            this.btnProses.Name = "btnProses";
+            this.btnProses.Size = new System.Drawing.Size(118, 37);
+            this.btnProses.TabIndex = 17;
+            this.btnProses.Text = "Proses";
+            this.btnProses.UseVisualStyleBackColor = true;
+            this.btnProses.Click += new System.EventHandler(this.btnProses_Click);
+            // 
+            // btnBersih
+            // 
+            this.btnBersih.Location = new System.Drawing.Point(350, 423);
+            this.btnBersih.Name = "btnBersih";
+            this.btnBersih.Size = new System.Drawing.Size(118, 37);
+            this.btnBersih.TabIndex = 18;
+            this.btnBersih.Text = "Bersih";
+            this.btnBersih.UseVisualStyleBackColor = true;
+            this.btnBersih.Click += new System.EventHandler(this.btnBersih_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Location = new System.Drawing.Point(580, 423);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(118, 37);
+            this.btnKeluar.TabIndex = 19;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnBersih);
+            this.Controls.Add(this.btnProses);
+            this.Controls.Add(this.cmbJabatan);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.rb_lk);
+            this.Controls.Add(this.rb_pr);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtNIK);
+            this.Controls.Add(this.txtAgama);
+            this.Controls.Add(this.txtTempat);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtTempat;
+        private System.Windows.Forms.TextBox txtAgama;
+        private System.Windows.Forms.TextBox txtNIK;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.RadioButton rb_pr;
+        private System.Windows.Forms.RadioButton rb_lk;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox cmbJabatan;
+        private System.Windows.Forms.Button btnProses;
+        private System.Windows.Forms.Button btnBersih;
+        private System.Windows.Forms.Button btnKeluar;
+    }
+}
+
---
+++ Input Biodata Karyawan/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Input_Biodata_Karyawan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "BIODATA KARYAWAN";
+            this.BackColor = Color.AliceBlue;
+            cmbJabatan.Items.Add("Direktur Personalia");
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(txtNama.Text);
+            listBox1.Items.Add(txtTempat);
+            listBox1.Items.Add(txtAgama);
+            listBox1.Items.Add(txtNIK);
+            listBox1.Items.Add(txtID);
+
+            if (rb_pr.Checked == true)
+                listBox1.Items.Add(rb_pr.Text);
+            else if (rb_lk.Checked == true)
+                listBox1.Items.Add(rb_lk.Text);
+            listBox1.Items.Add(cmbJabatan.Text);
+        }
+
+        private void btnBersih_Click(object sender, EventArgs e)
+        {
+            txtNama.Text = "";
+            txtTempat.Text = "";
+            txtAgama.Text = "";
+            txtNIK.Text = "";
+            txtID.Text = "";
+            rb_pr.Checked = false;
+            rb_lk.Checked = false;
+            listBox1.Items.Clear();
+            txtNama.Focus();
+            txtTempat.Focus();
+            txtAgama.Focus();
+            txtNIK.Focus();
+            txtID.Focus();
+        
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
